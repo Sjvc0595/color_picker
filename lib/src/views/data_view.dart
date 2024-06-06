35e83faf2a18a401c99c580fc94497f0151bfa2d
Samuel Vargas <sjvc0595@gmail.com>
2024-06-06 14:26:09 -0500

    Creation of data_view

---
+++ lib/src/views/data_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/bluetooth_service.dart';
+import 'bluetooth_settings_view.dart';
+
+import 'package:permission_handler/permission_handler.dart';
+
+class DataView extends StatefulWidget {
+  const DataView({super.key});
+
+  static const routeName = '/';
+
+  @override
+  State<DataView> createState() => _DataViewState();
+}
+
+class _DataViewState extends State<DataView> {
+  void _requestPermission() async {
+    await Permission.location.request();
+    await Permission.bluetooth.request();
+    await Permission.bluetoothScan.request();
+    await Permission.bluetoothConnect.request();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bluetoothService = Provider.of<BluetoothService>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Data'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.restorablePushNamed(
+                  context, BluetoothSettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(bluetoothService.isConnected
+                ? "Estás conectado"
+                : "No estás conectado"),
+            const Text('Data View'),
+          ],
+        ),
+      ),
+    );
+  }
+}
