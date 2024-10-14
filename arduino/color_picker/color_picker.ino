9c540162f23ff208b7da76042cec6e4538e6518d
Samuel Vargas <sjvc0595@gmail.com>
2024-10-13 21:07:28 -0500

    Refactor Arduino code for color picker functionality

--- arduino/color_picker/color_picker.ino
+++ arduino/color_picker/color_picker.ino
@@ -1,47 +1,53 @@
 #include <SoftwareSerial.h>
 
+// Ports for RGB LED
 int red = 11;
 int green = 10;
 int blue = 9;
 
+// String to store color
 String color = "";
 
 SoftwareSerial BT (3, 4); // RX, TX
 
 void setup()
 {
-	pinMode(13, OUTPUT);
-    Serial.begin(9600);
-    BT.begin(9600);
+	pinMode(13, OUTPUT); // onboard LED
+    Serial.begin(9600); // Serial monitor
+    BT.begin(9600); // Bluetooth
 }
 
 void loop()
 {
+    // Blink onboard LED
     if(Serial.available()){
         color = Serial.readString();
         processColor(color);
     }
 
+    // Check if BT is available
     if(BT.available()){
         Serial.println("BT available");
-        color = strip(BT.readString());
-        if(color.length() == 1){
-            if(color == "1"){
-                analogWrite(red, 100);
-            }else if(color == "0"){
-                analogWrite(red, 0);
+        color = strip(BT.readString()); // Read color from BT
+        if(color.length() == 1){ // Check if color is a single digit
+            if(color == "1"){ // Check if color is 1
+                analogWrite(red, 100); // Set red to 100
+            }else if(color == "0"){ // Check if color is 0
+                analogWrite(red, 0); // Set red to 0
             }
-        }else{
+        }else{ // If color is not a single digit
             processColor(color);
         }        
     }
 }
 
+// Function to convert hex to decimal
 int hexToDec(String hexString){
     int decValue = strtol(hexString.c_str(), NULL, 16);
     return decValue;
 }
 
+// Function to create color
 void createColor(int r, int g, int b){
     Serial.println(b);
     analogWrite(red, r);
@@ -49,6 +55,7 @@ void createColor(int r, int g, int b){
     analogWrite(blue, b);
 }
 
+// Function to process color
 void processColor(String color){
     int r = hexToDec(color.substring(0, 2));
     int g = hexToDec(color.substring(2, 4));
@@ -56,6 +63,7 @@ void processColor(String color){
     createColor(r, g, b);
 }
 
+// Function to strip non-ASCII characters
 String strip(String str) {
     String newStr = "";
     for (int i = 0; i < str.length(); i++) {
